ab3f6ddf73457c18a9a9e838df4ef34e886d834a
frozobeta <frozoweb@yahoo.com>
2017-03-10 22:49:37 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - InfinityGreece Academy</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700|Roboto:300,300i,400,400i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,900" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="desktop/login.css">
+    <link rel="stylesheet" type="text/css" href="desktop/general.css">
+    <link rel="stylesheet" type="text/css" href="desktop/navigation.css">
+    <link rel="stylesheet" type="text/css" href="desktop/style.css">
+
+ <!-- <script>
+    setInterval(function(){
+      window.history.go(0);
+    }, 3000);
+  </script>-->
+  <script>
+$(document).ready(function(){
+
+$(".wrap").click(function(){
+$(".top").toggleClass("addedtop");
+$(".mid").toggleClass("addedmid");
+$(".bot").toggleClass("addedbot");
+$(".menu").toggleClass("triggered");
+$("fade").toggleClass("fadeblack");
+
+
+});
+
+
+
+}); //final
+</script>
+  </head>
+  <body>
+    <bg></bg>
+    <gradient></gradient>
+    <fade></fade>
+    <block></block>
+    <header>
+    <div class="content">
+      <div class="navbar">  <div class="logo">InfinityGreece <span>Academy</span></div></div>
+
+        <div class="wrap">
+<span class="line top"></span>
+<span class="line mid"></span>
+<span class="line bot"></span>
+</div>
+
+          <nav class="menu">
+                <a href="index.php" class="nav-btn"><span>Home</span></a>
+                <a href="login.php" class="nav-btn"><span>Login</span></a>
+                <a href="register.php" class="nav-btn"><span>Sign up</span></a>
+                      </nav>
+      </div>
+  </header>
+
+  <form action="#" method="post" class="input-form">
+    <h1 class="page-title">Login</h1>
+    <input type="email" class="text-input" placeholder="Enter your email">
+    <input type="password" class="text-input" placeholder="Enter your password">
+    <input type="submit" class="submit-button" value="Login">
+  </form>
+  <p class="form-problem-text">Ο κωδικός δεν είναι σωστός.</p>
+  <p class="already-have-account">Δεν έχεις λογαριασμό; <a href="login.php">Κάνε εγγραφή εδώ!</a></p>
+  <footer>
+    <span class="footer-buttons">
+      <a href="#" class="footer-btn">Contact</a>
+      <a href="#" class="footer-btn">Privacy Policy</a>
+      <a href="#" class="footer-btn">About</a>
+
+
+    </span>
+    <span class="copyright-text">Copyright &copy; InfinityGreece</span>
+
+  </footer>
+  </body>
+</html>
---
+++ videopage.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recent Courses - InfinityGreece Academy</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700|Roboto:100,300,300i,400,400i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,900" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="desktop/general.css">
+    <link rel="stylesheet" type="text/css" href="desktop/navigation.css">
+    <link rel="stylesheet" type="text/css" href="desktop/style.css">
+    <link rel="stylesheet" type="text/css" href="desktop/main.css">
+    <link rel="stylesheet" type="text/css" href="desktop/profilepage.css">
+    <link rel="stylesheet" type="text/css" href="desktop/videopage.css">
+
+
+ <!-- <script>
+    setInterval(function(){
+      window.history.go(0);
+    }, 3000);
+  </script>-->
+  <script>
+$(document).ready(function(){
+
+$(".wrap").click(function(){
+$(".top").toggleClass("addedtop");
+$(".mid").toggleClass("addedmid");
+$(".bot").toggleClass("addedbot");
+$(".menu").toggleClass("triggered");
+$("fade").toggleClass("fadeblack");
+
+
+});
+
+
+
+}); //final
+</script>
+  </head>
+  <body>
+    <fade></fade>
+    <bg></bg>
+    <block></block>
+    <header>
+    <div class="content">
+      <div class="navbar">  <div class="logo">InfinityGreece <span>Academy</span></div></div>
+
+        <div class="wrap">
+          <span class="line top"></span>
+          <span class="line mid"></span>
+          <span class="line bot"></span>
+        </div>
+
+        <nav class="menu">
+                <a href="index.php" class="nav-btn"><span>Home</span></a>
+                <a href="login.php" class="nav-btn"><span>My Courses</span></a>
+                <a href="register.php" class="nav-btn"><span>My Profile</span></a>
+                <a href="register.php" class="nav-btn"><span>Log Out</span></a>
+        </nav>
+    </div>
+  </header>
+
+  <h1 class="page-title">
+    <span class="page-title-span"><i>Course:</i></span>
+    Wordpress
+  </h1>
+<div class="main-wrap">
+  <p class="your-progress">Your progress: <span class="your-progress-per">65%</span>
+</div>
+<div class="video-wrap">
+  <iframe src="https://www.youtube.com/embed/tjg3-Wla3PU" frameborder="0" allowfullscreen></iframe>
+</div>
+  <!-- <footer>
+
+    <span class="copyright-text">Copyright &copy; InfinityGreece</span>
+    <span class="footer-buttons">
+      <a href="#" class="footer-btn">Contact</a>
+      <a href="#" class="footer-btn">Privacy Policy</a>
+      <a href="#" class="footer-btn">About</a>
+
+
+    </span>
+  </footer> -->
+  </body>
+</html>
